ea25792eebdd843d569482171929e36b723e0155
p.devv <pd27052003@gmail.com>
2024-10-30 03:14:04 +0530

    build hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
 
     <>
-    <h1 className="heading">Home</h1>
+    <section className="pink_container">
+          <h1 className="heading">Pitch Your StartUp,<br /> Connect with Entrepreneurs</h1>
+          <p className="sub-heading !max-w-3xl">Submit ideas,Vote on pitches, and Get Noticed in virtual competitions.
+            
+          </p>
+
+    </section>
     </>
 
   
