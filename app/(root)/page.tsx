0cac08f374e813156c69a8d02bc0c64f5777605d
p.devv <pd27052003@gmail.com>
2024-10-30 13:57:38 +0530

    implemented startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,24 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
 import { promises } from "dns";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}:{searchParams:Promise<{query?:string}>}) {
   
   const query=(await searchParams).query
+
+  const posts=[
+    {
+      _createdAt:new Date(),
+      views:55,
+      author:{_id:1,name:"dev prajapati"},
+      _id:1,
+       description:'This is a description',
+       image:"https://www.kasandbox.org/programming-images/avatars/mr-pants-purple.png",
+       category:"Robots",
+       title:"We Robots",
+    },
+  ]
 
 
   return (
@@ -17,6 +31,19 @@ export default async function Home({searchParams}:{searchParams:Promise<{query?:
 
           <SearchForm query={query}/>
 
+    </section>
+    <section className="section_container">
+      <p className="text-30-semibold">
+        {query ? `Search results for "${query}"` : 'All Startups '}
+      </p>
+
+      <ul className="mt-7 card_grid">
+        {posts?.length> 0 ?(
+          posts.map((post:StartupCardType)=>(<StartupCard key={post?._id} post={post}/>))
+
+        ):(<p className="no-results">No Startup Found</p>)
+         }
+      </ul>
     </section>
     </>
 
